fbe87ba1aa4451ff01518eb1be0532d577171b51
Ken Kendall <kendall@okcupidlabs.com>
2012-11-21 21:03:17 -0800

    :baby: remove the header but put a title tag back in

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
 
 class MobileFilter < Sinatra::Base
   get '/' do
-    body 'hello, world'
+    url = params[:url]
+    url = 'http://'+url unless url.index('http://') == 0
+    doc = Nokogiri::HTML(open(url))
+    a = doc
+
+    title = doc.search('title').text
+    title_node = Nokogiri::XML::Node.new 'title', a
+    title_node.content = title
+    a.search('head').remove
+
+    a.children.last.children.first.add_previous_sibling(title_node)
+
+    body a.to_html
   end
 end
